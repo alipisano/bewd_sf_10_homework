69b93b849e8419b81af53929afa789575d4c887a
Alexandra Pisano <pisano.alexandra@gmail.com>
2015-09-28 20:32:58 -0700

    Controller set up

---
+++ AliPisano-alipisano/final_project/prank_app/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+#create a messages resource- CRUD operations
+resource :messages
+
+# tells Rails to map requests to http://localhost:3000/home to the static page home controller
+get 'home', to: "static_pages#home"
+
+#tell Rails to map requests to the root of the application to the static page home controller
+root 'static_pages#home'
+
+end
